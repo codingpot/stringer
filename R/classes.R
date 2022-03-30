f08cc032b133572197fba00d6a1214fdf56c848f
snowmerak <snowmerak@outlook.com>
2022-03-31 07:28:46 +0900

    distribute interface file

--- R/classes.R
+++ R/classes.R
@@ -15,12 +15,3 @@ text <- function(x) {
 regex <- function(x) {
     new("regex", value = x)
 }
-
-str_count <- function(l, pattern) {
-    switch(
-        class(pattern),
-        text = str_text_count(l, pattern@value),
-        regex = str_regex_count(l, pattern@value),
-        character = str_regex_count(l, pattern)
-    )
-}
