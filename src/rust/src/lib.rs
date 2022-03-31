7aca33c58ea9acf163395892f04cc3aefa21d1af
snowmerak <snowmerak@outlook.com>
2022-04-01 07:14:27 +0900

    add locate functions

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod utf8;
-pub mod find;
+pub mod locate;
 
 use extendr_api::prelude::*;
 
@@ -7,9 +7,79 @@ extendr_module! {
     mod stringer;
 
     fn str_length;
+
+    fn str_utf8_locate;
+    fn str_utf8_locates;
+    fn str_regex_locate;
+    fn str_regex_locates;
+    fn str_bytes_locate;
+    fn str_bytes_locates;
 }
 
 #[extendr]
-fn str_length(str: String) -> i64 {
+pub fn str_length(str: String) -> i64 {
     utf8::length_of(str) as i64
+}
+
+#[extendr]
+pub fn str_utf8_locate(str: String, sub: String) -> Vec<i64> {
+    let index = locate::single_utf8_str_locate(str, sub);
+    match index {
+        Some((start, end)) => vec![start as i64, end as i64],
+        None => vec![],
+    }
+}
+
+#[extendr]
+pub fn str_utf8_locates(str: String, sub: String) -> Vec<i64> {
+    let mut result = vec![];
+    let indexs = locate::multi_utf8_str_locate(str, sub);
+    for (_, v) in indexs.iter().enumerate() {
+        result.push(v.0 as i64);
+        result.push(v.1 as i64);
+    }
+    result
+}
+
+#[extendr]
+pub fn str_regex_locate(str: String, pat: String) -> Vec<i64> {
+    let index = locate::single_regex_str_locate(str, pat);
+    match index {
+        Ok((start, end)) => vec![start as i64, end as i64],
+        Err(err) => panic!("{}", err),
+    }
+}
+
+#[extendr]
+pub fn str_regex_locates(str: String, pat: String) -> Vec<i64> {
+    let mut result = vec![];
+    let indexs = match locate::multi_regex_str_locate(str, pat) {
+        Ok(indexs) => indexs,
+        Err(err) => panic!("{}", err),
+    };
+    for (_, v) in indexs.iter().enumerate() {
+        result.push(v.0 as i64);
+        result.push(v.1 as i64);
+    }
+    result
+}
+
+#[extendr]
+pub fn str_bytes_locate(str: String, sub: String) -> Vec<i64> {
+    let index = locate::single_bytes_str_locate(str, sub);
+    match index {
+        Some((start, end)) => vec![start as i64, end as i64],
+        None => vec![],
+    }
+}
+
+#[extendr]
+pub fn str_bytes_locates(str: String, sub: String) -> Vec<i64> {
+    let mut result = vec![];
+    let indexs = locate::multi_bytes_str_locate(str, sub);
+    for (_, v) in indexs.iter().enumerate() {
+        result.push(v.0 as i64);
+        result.push(v.1 as i64);
+    }
+    result
 }
