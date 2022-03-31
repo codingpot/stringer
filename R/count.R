1f5458b0d99a022ffa5e05c22231493a2107a40a
snowmerak <snowmerak@outlook.com>
2022-04-01 07:17:30 +0900

    remove codes

--- R/count.R
+++
@@ -1,8 +0,0 @@
-str_count <- function(l, pattern) {
-    switch(
-        class(pattern),
-        fixed = str_text_count(l, pattern@value),
-        regex = str_regex_count(l, pattern@value),
-        character = str_regex_count(l, pattern)
-    )
-}
