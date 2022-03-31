af4ab88e4eb522cb280dce617e38c0af6d77e937
snowmerak <snowmerak@outlook.com>
2022-04-01 07:17:37 +0900

    add coll class

--- R/classes.R
+++ R/classes.R
@@ -8,10 +8,22 @@ setClass(
     representation(value = "character")
 )
 
+setClass(
+    "coll",
+    representation(
+        value = "character",
+        locale = "character",
+    )
+)
+
 fixed <- function(x) {
     new("fixed", value = x)
 }
 
 regex <- function(x) {
     new("regex", value = x)
+}
+
+coll <- function(x, local="en") {
+    new("coll", value = x, locale = local)
 }
