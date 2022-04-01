86fd739862f0d863e984ccaa28a848a3efd8bd25
snowmerak <snowmerak@outlook.com>
2022-04-01 12:01:08 +0900

    add constructors

--- R/classes.R
+++ R/classes.R
@@ -30,10 +30,58 @@ fixed <- function(x, ignore_case = FALSE) {
     new("fixed", value = x, ignore_case = ignore_case)
 }
 
+get_fixed <- function(x) {
+    if (is.null(x)) {
+        return(NULL)
+    }
+
+    if (x@ignore_case) {
+        x@value <- convert_to_uppercase(x@value)
+    }
+
+    x@value
+}
+
 regex <- function(x, ignore_case = FALSE, multiline = FALSE, comments = FALSE, dot_all = FALSE) {
     new("regex", value = x, ignore_case = ignore_case, multiline = multiline, comments = comments, dot_all = dot_all)
 }
 
+get_regex <- function(x) {
+    if (is.null(x)) {
+        return(NULL)
+    }
+
+    if (x@dot_all) {
+        x@value <- add_s_flag_to_dot(x@value)
+    }
+
+    if (x@multiline) {
+        x@value <- add_m_flag_to(x@value)
+    }
+
+    if (x@comments) {
+        x@value <- add_x_flag_to(x@value)
+    }
+
+    if (x@ignore_case) {
+        x@value <- add_i_flag_to(x@value)
+    }
+
+    x@value
+}
+
 coll <- function(x, ignore_case = FALSE, locale = "en") {
     new("coll", value = x, ignore_case = ignore_case, locale = locale)
+}
+
+get_coll <- function(x) {
+    if (is.null(x)) {
+        return(NULL)
+    }
+
+    if (x@ignore_case) {
+        x@value <- convert_to_uppercase(x@value)
+    }
+
+    x@value
 }
